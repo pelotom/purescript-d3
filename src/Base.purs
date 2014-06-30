03acbf2ee6c070ced14292a9ffbd94cde4c03f52
Tom Crockett <pelotom@gmail.com>
2014-06-29 19:50:04 -0700

    Add (|.) operator
    Just a synonym for (>>=) but it makes writing in the "fluid interface" style
    more visually pleasant

--- src/Base.purs
+++ src/Base.purs
@@ -1,6 +1,7 @@
 module Graphics.D3.Base
   ( DOM()
   , D3Eff()
+  , (|.)
   , void
   ) where
 
@@ -10,6 +11,10 @@ import Control.Monad.Eff
 foreign import data DOM :: !
 
 type D3Eff e a = Eff (dom :: DOM | e) a
+
+-- Syntactic sugar to make chained monadic statements look similar to the
+-- "fluid interface" style of chained method calls in JavaScript
+(|.) = (>>=)
 
 -- Remove this once it's available in Prelude
 void :: forall f a. (Functor f) => f a -> f Unit
