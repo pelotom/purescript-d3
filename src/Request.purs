63826a1a6af750aaea75a9c8d5738b9d7aafdb27
erdeszt <erdeszt@gmail.com>
2015-01-29 23:23:19 +0100

    Use a less restrictive effect in tsv and json

--- src/Request.purs
+++ src/Request.purs
@@ -13,14 +13,14 @@ import Graphics.D3.Base
 
 type RequestError = { status :: Number, statusText :: String }
 
-tsv :: forall a. String -> (Either RequestError [Foreign] -> D3Eff a) -> D3Eff Unit
+tsv :: forall e a. String -> (Either RequestError [Foreign] -> Eff (d3 :: D3 | e) a) -> D3Eff Unit
 tsv = ff (\d -> d) Left Right
   where
   ff = unsafeForeignFunction
     ["acc", "Left", "Right", "url", "handle", ""]
     "d3.tsv(url, acc, function(error, data) { if (error) handle(Left(error))(); else handle(Right(data))(); })"
 
-json :: forall a. String -> (Either RequestError Foreign -> D3Eff a) -> D3Eff Unit
+json :: forall e a. String -> (Either RequestError Foreign -> Eff (d3 :: D3 | e) a) -> D3Eff Unit
 json = ff Left Right
   where
   ff = unsafeForeignFunction
