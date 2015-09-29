766d2a1c309dd0bc79a028a88d2f8b45fe1717f1
Tom Crockett <pelotom@gmail.com>
2015-09-29 00:45:22 -0700

    Indent gulpfile uniformly with spaces, not tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 'use strict'
 
-var gulp      	= require('gulp')
-  , purescript 	= require('gulp-purescript')
-  , concat 		 	= require('gulp-concat')
-	, rimraf 			= require('rimraf')
+var gulp        = require('gulp')
+  , purescript  = require('gulp-purescript')
+  , concat      = require('gulp-concat')
+  , rimraf      = require('rimraf')
   ;
 
 var paths = {
-	src: 'src/**/*.purs',
+  src: 'src/**/*.purs',
   ffi: 'src/**/*.js',
-	dest: 'build/output',
-	bowerSrc: 'bower_components/purescript-*/src/**/*.purs',
+  dest: 'build/output',
+  bowerSrc: 'bower_components/purescript-*/src/**/*.purs',
   bowerFfi:  'bower_components/purescript-*/src/**/*.js',
-	manualReadme: 'docsrc/README.md',
-	apiDest: 'build/API.md',
-	readmeDest: 'README.md'
+  manualReadme: 'docsrc/README.md',
+  apiDest: 'build/API.md',
+  readmeDest: 'README.md'
 };
 
 gulp.task('clean', function (cb) {
@@ -22,39 +22,39 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('compile', ['clean'], function() {
-	var psc = purescript.psc({
-		// Compiler options
+  var psc = purescript.psc({
+    // Compiler options
     src: [paths.src, paths.bowerSrc],
     ffi: [paths.ffi, paths.bowerFfi],
-		output: paths.dest
-	});
-	psc.on('error', function(e) {
-		console.error(e.message);
-		psc.end();
-	});
-	return psc;
+    output: paths.dest
+  });
+  psc.on('error', function(e) {
+    console.error(e.message);
+    psc.end();
+  });
+  return psc;
 });
 
 gulp.task('generateDocs', ['compile'], function() {
-	return purescript.pscDocs({
+  return purescript.pscDocs({
       src: [paths.src, paths.bowerSrc]
     })
-	  .pipe(gulp.dest(paths.apiDest))
-	  ;
+    .pipe(gulp.dest(paths.apiDest))
+    ;
 });
 
 gulp.task('concatDocs', ['generateDocs'], function () {
-	return gulp.src([paths.manualReadme, paths.apiDest])
-		.pipe(concat(paths.readmeDest))
-		.pipe(gulp.dest(''))
-		;
+  return gulp.src([paths.manualReadme, paths.apiDest])
+    .pipe(concat(paths.readmeDest))
+    .pipe(gulp.dest(''))
+    ;
 });
 
 gulp.task('docs', ['generateDocs', 'concatDocs']);
 
 gulp.task('watch', ['build'], function() {
-	gulp.watch(paths.src, ['build']);
-	gulp.watch(paths.manualReadme, ['concatDocs']);
+  gulp.watch(paths.src, ['build']);
+  gulp.watch(paths.manualReadme, ['concatDocs']);
 });
 
 gulp.task('build', ['compile', 'docs']);
