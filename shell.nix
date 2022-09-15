4dac5b2a844df4edfc7af166305f69fd4b5a2df5
Przemek Kaminski <pk@intrepidus.pl>
2022-09-15 11:46:26 +0200

    More work on d3 upgrading to 0.15.4

--- shell.nix
+++ shell.nix
@@ -1,19 +1,22 @@
-{ pkgs ? import ./nix/pinned.nix { } }:
 let
-  easy-ps = import ./nix/easy-ps.nix { inherit pkgs; };
-
-  build = pkgs.writeShellScriptBin "build" ''
-    #!/usr/bin/env bash
-    purs compile "src/**/*.purs" "test/**/*.purs"
-  '';
+  pkgs = import <nixpkgs> {};
+  default-shell = import (
+    pkgs.fetchFromGitHub {
+      owner = "garganscript";
+      repo = "package-sets";
+      rev = "master";
+      sha256 = "1z7x5g0ba62l6hiagwsmnmdg07mz5xwf35qvb9p7fry2lks1ma18";
+    } + "/default-shell.nix");
 in
 pkgs.mkShell {
-  name = "purescript-reactix-d3";
+  name = "purescript-d3";
 
   buildInputs = [
-    easy-ps.purs-0_15_4
-    easy-ps.psc-package
-    build
-    pkgs.dhall-json
+    default-shell.purs
+    default-shell.easy-ps.psc-package
+    default-shell.easy-ps.spago
+    default-shell.build
+    default-shell.pkgs.dhall-json
+    pkgs.esbuild
   ];
 }
--- src/Base.js
+++ src/Base.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var window = window || undefined;
-import * as d3 from 'd3/dist/d3.js';
+import * as d3 from 'd3';
 
 if (window !== undefined) {
   window.d3 = d3;
 }
 
-export const d3;
+export { d3 };
